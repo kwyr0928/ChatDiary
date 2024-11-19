6d17343d37debd0d5072eed810b7fd63e8e82c96
nazunanana <azukiii1953@gmail.com>
2024-11-19 15:14:02 +0900

    'shadcn/uiとreact-icons導入、UIのtest用ページ作成(app/test)'

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button"
+
+export default function Page() {
+    return (
+        <div>
+            <div>こんにちは</div>
+            <Button className="bg-pink-500 hover:bg-pink-600">あああ</Button>
+        </div>
+    )
+}
