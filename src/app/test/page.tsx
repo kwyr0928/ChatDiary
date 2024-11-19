98f6894b1b70000f6c3aea1a46e995a7720741ad
nazunanana <azukiii1953@gmail.com>
2024-11-19 17:01:21 +0900

    'インプット、チャット欄、可変テキストエリア、タグUI作成'

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,46 @@
-import { Button } from "~/components/ui/button"
+import { IoSendSharp } from "react-icons/io5";
+import ResizeTextarea from "~/components/resizeTextarea";
+import Tag from "~/components/tag";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export default function Page() {
     return (
-        <div>
-            <div>こんにちは</div>
-            <Button className="bg-pink-500 hover:bg-pink-600">あああ</Button>
+        <div className="h-screen bg-red-50">
+            {/* ボタンUI */}
+            <div className="mb-5">
+                <Label className="text-lg block">ボタン</Label>
+                <Button className="bg-red-400 hover:bg-red-500">塗りつぶしボタン</Button>
+                <Button className="bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent">枠つきボタン</Button>
+            </div>
+
+            {/* インプット */}
+            <div className="mb-5">
+                <Label className="text-lg block">入力欄</Label>
+                <Input />
+            </div>
+
+            {/* チャット欄 */}
+            <div className="mb-5">
+                <Label className="text-lg block">チャット</Label>
+                <div className="flex space-x-2 mb-3 items-end">
+                    <textarea rows={4} className="resize-none border rounded focus:outline-none" />
+                    <IoSendSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                </div>
+            </div>
+
+            {/* 可変テキストエリア */}
+            <div className="mb-5">
+                <Label className="text-lg block">高さが変わるテキストエリア</Label>
+                <ResizeTextarea className="resize-none border rounded focus:outline-none mb-3" />
+            </div>
+
+            {/* タグ(見た目のみ) */}
+            <div className="mb-5">
+                <Label className="text-lg block">タグ</Label>
+                <Tag text="新規タグ"/>
+            </div>
         </div>
     )
 }
---
+++ src/components/tag.tsx
@@ -0,0 +1,14 @@
+import { IoRemoveCircleSharp } from "react-icons/io5";
+
+export default function Tag(props: {text: string}) {
+    const { text } = props
+    return (
+        <span id="badge-dismiss-1" className="inline-flex items-center px-2 py-1 font-semibold text-red-400 bg-white rounded-full border border-red-400 mb-3">
+            <button data-dismiss-target="#badge-dismiss-1">
+                {/* onClick={removeTag} */}
+                <IoRemoveCircleSharp size="24px" className="text-red-400" />
+            </button>
+            <span className="pt-0.5 px-1 text-base leading-none">{text}</span>
+        </span>
+    )
+}
