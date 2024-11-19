25015cfd5d86f9ca5651febffb8fcdebc4d23ff9
uyuuuuu <soufflesumire.03@gmail.com>
2024-11-19 20:12:23 +0900

    feat: putpost zod

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,4 +1,5 @@
 // 構造体定義ファイル
+// Route Handler
 import { z } from "zod";
 
 /*
@@ -7,7 +8,7 @@ import { z } from "zod";
 export const user = z.object({
   id: z.string().min(1).optional(),
   name: z.string().min(1),
-  email: z.string().min(3),
+  email: z.string().email(),
   passward: z.string().min(1), //TODO: 暗号化する
   created_at: z.date().optional(),
 });
@@ -57,10 +58,61 @@ export const analyses = z.object({
 
 
 /*
-  POST/PUTデータの構造体
+  POST/PUTデータの構造体 - アカウント関連
 */
 
+// /api/user/signup
+export const postSignup = z.object({
+  email: z.string().email(),
+  passward: z.string().min(1),
+});
 
+// /api/user/signin
+export const postSignin = z.object({
+  email: z.string().email(),
+  passward: z.string().min(1),
+});
+
+// /api/user/signout
+export const postSignout = z.object({
+  userId: z.string(),
+});
+
+// api/user/remail
+export const postReEmail = z.object({
+  email: z.string().email(),
+});
+
+/*
+  POST/PUTデータの構造体 - 日記関連
+*/
+
+// api/diary/[id]
+export const putDiary = z.object({
+  tags: z.array(z.number()),
+  text: z.string(),
+  isPublish: z.boolean()
+});
+
+// api/chat
+export const postChat = z.object({
+  userId: z.string(),
+});
+
+// api/chat/[id]
+export const putChat = z.object({
+  mode: z.number(),
+});
+
+// api/chat/[id]/send
+export const postSendChat = z.object({
+  mode: z.number(),
+});
+
+// api/diary/[id]/new
+export const postDiary = z.object({
+  mode: z.number(),
+});
 
 /*
   GETデータの構造体
