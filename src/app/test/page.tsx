b1362ba1aaffb5311486e476bffc6ad7dd2cf437
nazunanana <azukiii1953@gmail.com>
2024-11-20 14:20:45 +0900

    'ラジオボタン追加、チャット送信ボタンの色変更'

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,7 @@ import Tag from "~/components/tag";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 
 /* UI表示テスト用ページ */
 export default function Page() {
@@ -12,7 +13,7 @@ export default function Page() {
             {/* ボタンUI */}
             <div className="mb-5">
                 <Label className="text-lg block">ボタン</Label>
-                <Button className="bg-red-400 hover:bg-red-500">塗りつぶしボタン</Button>
+                <Button className="bg-red-400 hover:bg-rose-500">塗りつぶしボタン</Button>
                 <Button className="bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent">枠つきボタン</Button>
             </div>
 
@@ -27,7 +28,7 @@ export default function Page() {
                 <Label className="text-lg block">チャット</Label>
                 <div className="flex space-x-2 mb-3 items-end">
                     <textarea rows={4} className="resize-none border rounded focus:outline-none" />
-                    <IoSendSharp style={{ color: '#5c5c5c', fontSize: '25px' }} />
+                    <IoSendSharp style={{ color:"#f87171", fontSize: '25px' }} />
                 </div>
             </div>
 
@@ -40,7 +41,22 @@ export default function Page() {
             {/* タグ(見た目のみ) */}
             <div className="mb-5">
                 <Label className="text-lg block">タグ</Label>
-                <Tag text="新規タグ"/>
+                <Tag text="新規タグ" />
+            </div>
+
+            {/* ラジオボタン */}
+            <div className="mb-5">
+                <Label className="text-lg block">ラジオボタン</Label>
+                <RadioGroup defaultValue="private">
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="public" id="public" className="border-red-400" />
+                        <Label htmlFor="public">公開</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="private" id="private" className="border-red-400" />
+                        <Label htmlFor="private">非公開</Label>
+                    </div>
+                </RadioGroup>
             </div>
         </div>
     )
