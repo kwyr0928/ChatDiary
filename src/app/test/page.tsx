03f0db6f1d11cf1b5e2231bae30051ab1bf4a9d9
nazunanana <azukiii1953@gmail.com>
2024-11-20 14:54:52 +0900

    'カード追加'

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,6 +2,7 @@ import { IoSendSharp } from "react-icons/io5";
 import ResizeTextarea from "~/components/resizeTextarea";
 import Tag from "~/components/tag";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
@@ -9,54 +10,68 @@ import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 /* UI表示テスト用ページ */
 export default function Page() {
     return (
-        <div className="h-screen bg-red-50">
-            {/* ボタンUI */}
-            <div className="mb-5">
-                <Label className="text-lg block">ボタン</Label>
-                <Button className="bg-red-400 hover:bg-rose-500">塗りつぶしボタン</Button>
-                <Button className="bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent">枠つきボタン</Button>
-            </div>
-
-            {/* インプット */}
-            <div className="mb-5">
-                <Label className="text-lg block">入力欄</Label>
-                <Input />
-            </div>
-
-            {/* チャット欄 */}
-            <div className="mb-5">
-                <Label className="text-lg block">チャット</Label>
-                <div className="flex space-x-2 mb-3 items-end">
-                    <textarea rows={4} className="resize-none border rounded focus:outline-none" />
-                    <IoSendSharp style={{ color:"#f87171", fontSize: '25px' }} />
+        // 背景: red-50
+        <div className="flex items-center justify-center h-screen bg-red-50">
+            <div className="w-full max-w-md flex items-start flex-col">
+                {/* ボタンUI */}
+                <div className="mb-5">
+                    <Label className="text-lg block">ボタン</Label>
+                    <Button className="bg-red-400 hover:bg-rose-500">塗りつぶしボタン</Button>
+                    <Button className="bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent">枠つきボタン</Button>
                 </div>
-            </div>
 
-            {/* 可変テキストエリア */}
-            <div className="mb-5">
-                <Label className="text-lg block">高さが変わるテキストエリア</Label>
-                <ResizeTextarea className="resize-none border rounded focus:outline-none mb-3" />
-            </div>
+                {/* インプット */}
+                <div className="mb-5">
+                    <Label className="text-lg block">入力欄</Label>
+                    <Input />
+                </div>
 
-            {/* タグ(見た目のみ) */}
-            <div className="mb-5">
-                <Label className="text-lg block">タグ</Label>
-                <Tag text="新規タグ" />
-            </div>
-
-            {/* ラジオボタン */}
-            <div className="mb-5">
-                <Label className="text-lg block">ラジオボタン</Label>
-                <RadioGroup defaultValue="private">
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="public" id="public" className="border-red-400" />
-                        <Label htmlFor="public">公開</Label>
+                {/* チャット欄 */}
+                <div className="mb-5">
+                    <Label className="text-lg block">チャット</Label>
+                    <div className="flex space-x-2 mb-3 items-end">
+                        <textarea rows={4} className="resize-none border rounded focus:outline-none" />
+                        <IoSendSharp style={{ color: "#f87171", fontSize: '25px' }} />
                     </div>
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="private" id="private" className="border-red-400" />
-                        <Label htmlFor="private">非公開</Label>
-                    </div>
-                </RadioGroup>
+                </div>
+
+                {/* 可変テキストエリア */}
+                <div className="mb-5">
+                    <Label className="text-lg block">高さが変わるテキストエリア</Label>
+                    <ResizeTextarea className="resize-none border rounded focus:outline-none mb-3" />
+                </div>
+
+                {/* タグ(見た目のみ) */}
+                <div className="mb-5">
+                    <Label className="text-lg block">タグ</Label>
+                    <Tag text="新規タグ" />
+                </div>
+
+                {/* ラジオボタン */}
+                <div className="mb-5">
+                    <Label className="text-lg block">ラジオボタン</Label>
+                    <RadioGroup defaultValue="private">
+                        <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="public" id="public" className="border-red-400" />
+                            <Label htmlFor="public">公開</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="private" id="private" className="border-red-400" />
+                            <Label htmlFor="private">非公開</Label>
+                        </div>
+                    </RadioGroup>
+                </div>
+
+                {/* カード */}
+                <div className="mb-5">
+                    <Label className="text-lg block">カード</Label>
+                    {/* wは仮 */}
+                    <Card className="w-[350px] shadow-none">
+                        <CardContent className="p-3">
+                            出力された日記、チャット文、フィードバックなどを表示する際の白枠
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     )
