c2c70f395b0c7eb0a21149e2010e8b134065dc6e
niiroo <114127299+niiroo@users.noreply.github.com>
2024-12-04 23:58:15 +0900

    feat: Geminiからのレスポンスが返ってくる

--- src/app/api/aitest/route.ts
+++ src/app/api/aitest/route.ts
@@ -5,19 +5,37 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function POST(req: Request) {
     try {
         const { prompt_post } = await req.json();
-        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || 'default_api_key');  // default値を設定
+        // Gemini APIキーを設定
+        // const apiKey = process.env.GEMINI_API_KEY;
+        const apiKey = "AIzaSyD7UYVUhhJFBP3SVwzr92EmaIB5T5lw8gU";
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const result = await model.generateContentStream(prompt_post);
-        const response = await result.response
+        if (!apiKey) {
+            return NextResponse.json({ error: 'API key not found' }, { status: 500 });
+        }
+        const genAI = new GoogleGenerativeAI(apiKey);
+        // モデルの取得
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });  // 使用モデル指定
+        // テキスト生成
+        const result = await model.generateContent({ 
+            contents: [{ role: 'USER', parts: [{ text: prompt_post }] }], 
+            generationConfig: { maxOutputTokens: 100 }, 
+        });
+        // レスポンスの取得
+        const response = await result.response;
+        const generatedText = await response.text();
 
+        console.log("プロンプト:", prompt_post);
+        console.log("APIレスポンス:", response);
+        console.log(JSON.stringify(response.text));
+        // console.log(response.candidates?.[0]?.content?.text); // contentプロパティのtextプロパティを取得
+        
         return NextResponse.json({
-            message: response.text()
+            message: generatedText
         })
     } catch (error) {
-        console.error("Error in POST xxx request:", error);
+        console.error("エラーが発生しました:", error);
         return NextResponse.json(
-            { error: "Internal server error" },
+            { error: "テキスト生成中にエラーが発生しました。" },
             { status: 500 },
         );
     }
