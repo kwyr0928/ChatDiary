cc9a94376cdd6b3a98274dab46fbead3ad7e70bf
nazunanana <azukiii1953@gmail.com>
2024-12-05 21:49:30 +0900

    diary/editのtextareaをresizeTextareaにした

--- src/app/diary/edit/page.tsx
+++ src/app/diary/edit/page.tsx
@@ -12,6 +12,7 @@ import {
   IoPersonCircleSharp,
   IoTrashSharp,
 } from "react-icons/io5";
+import ResizeTextarea from "~/components/resizeTextarea";
 import Tag from "~/components/tag";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
@@ -30,89 +31,84 @@ export default function Page() {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-md flex-col items-center justify-center bg-red-50 text-gray-600">
       <div className="flex w-full items-center justify-around pt-5 text-center">
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogTrigger onClick={() => setIsOpen(true)} className="">
-              <IoChevronBackSharp color="#f87171" size={"30px"} />
-            </DialogTrigger>
-            <DialogContent className="w-[80%]">
-          <DialogHeader>
-            <DialogTitle className="mt-5">編集内容を削除して戻りますか？</DialogTitle>
-          </DialogHeader>
-          <DialogDescription className="text-center text-gray-500">
-            編集内容は反映されません
-          </DialogDescription>
-          <div className="flex justify-around">
-            <div className="my-2">
-              <Button
-                className="w-[100px] rounded-full bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent"
-                onClick={() => setIsOpen(false)}
-              >
-                いいえ
-              </Button>
-            </div>
-            <Link href={"/diary/detail"}>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+          <DialogTrigger onClick={() => setIsOpen(true)} className="">
+            <IoChevronBackSharp color="#f87171" size={"30px"} />
+          </DialogTrigger>
+          <DialogContent className="w-[80%]">
+            <DialogHeader>
+              <DialogTitle className="mt-5">編集内容を削除して戻りますか？</DialogTitle>
+            </DialogHeader>
+            <DialogDescription className="text-center text-gray-500">
+              編集内容は反映されません
+            </DialogDescription>
+            <div className="flex justify-around">
               <div className="my-2">
-                <Button className="w-[100px] rounded-full bg-red-400 hover:bg-rose-500">
-                  はい
+                <Button
+                  className="w-[100px] rounded-full bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent"
+                  onClick={() => setIsOpen(false)}
+                >
+                  いいえ
                 </Button>
               </div>
-            </Link>
-          </div>
-        </DialogContent>
-      </Dialog>
+              <Link href={"/diary/detail"}>
+                <div className="my-2">
+                  <Button className="w-[100px] rounded-full bg-red-400 hover:bg-rose-500">
+                    はい
+                  </Button>
+                </div>
+              </Link>
+            </div>
+          </DialogContent>
+        </Dialog>
         <p className="text-lg text-gray-700">2024/10/2</p>
         <Dialog open={isOpen2} onOpenChange={setIsOpen2}>
-            <DialogTrigger onClick={() => setIsOpen2(true)} className="">
-              <IoTrashSharp color="gray" size={"35px"} />
-            </DialogTrigger>
-            <DialogContent className="w-[80%]">
-          <DialogHeader>
-            <DialogTitle className="mt-5">日記を削除しますか？</DialogTitle>
-          </DialogHeader>
-          <DialogDescription className="text-center text-gray-500">
-            この操作は元に戻せません
-          </DialogDescription>
-          <div className="flex justify-around">
-            <div className="my-2">
-              <Button
-                className="w-[100px] rounded-full bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent"
-                onClick={() => setIsOpen2(false)}
-              >
-                いいえ
-              </Button>
-            </div>
-            <Link href={"/home"}>
+          <DialogTrigger onClick={() => setIsOpen2(true)} className="">
+            <IoTrashSharp color="gray" size={"35px"} />
+          </DialogTrigger>
+          <DialogContent className="w-[80%]">
+            <DialogHeader>
+              <DialogTitle className="mt-5">日記を削除しますか？</DialogTitle>
+            </DialogHeader>
+            <DialogDescription className="text-center text-gray-500">
+              この操作は元に戻せません
+            </DialogDescription>
+            <div className="flex justify-around">
               <div className="my-2">
-                <Button className="w-[100px] rounded-full bg-red-400 hover:bg-rose-500" onClick={() => {
-        toast({
-          title: "日記を削除しました。",
-        })
-      }}>
-                  はい
+                <Button
+                  className="w-[100px] rounded-full bg-white hover:bg-red-400 text-red-400 hover:text-white border border-red-400 hover:border-transparent"
+                  onClick={() => setIsOpen2(false)}
+                >
+                  いいえ
                 </Button>
               </div>
-            </Link>
-          </div>
-        </DialogContent>
-      </Dialog>
+              <Link href={"/home"}>
+                <div className="my-2">
+                  <Button className="w-[100px] rounded-full bg-red-400 hover:bg-rose-500" onClick={() => {
+                    toast({
+                      title: "日記を削除しました。",
+                    })
+                  }}>
+                    はい
+                  </Button>
+                </div>
+              </Link>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="mb-auto mt-5 w-[85%]">
         <div className="flex items-center justify-start space-x-5">
           <p className="my-2 text-lg">日記本文</p>
           <Link href={"/diary/edit"}>
-            <IoCheckmarkSharp size={"23px"} color="#f87171"  onClick={() => {
-        toast({
-          title: "保存しました。",
-        })
-      }}/>
+            <IoCheckmarkSharp size={"23px"} color="#f87171" onClick={() => {
+              toast({
+                title: "保存しました。",
+              })
+            }} />
           </Link>
         </div>
-        {/* 可変テキストエリアにしたい */}
-          <textarea
-                className="w-full text-gray-600  h-36 px-5 py-3 rounded border border-gray-300 p-2"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
+        <ResizeTextarea className="resize-none focus:outline-none w-full text-gray-600  h-36 px-5 py-3 rounded border border-gray-300 p-2" text={text} onChange={setText} />
         <p className="mb-2 mt-5 text-left text-lg">タグ</p>
         <div className="flex gap-3">
           <Tag text="food" />
@@ -147,26 +143,26 @@ export default function Page() {
         </div>
         <p className="my-2 text-lg">チャットログ</p>
         <div className="mb-auto">
-        {/* カード */}
-        <Card className="mb-5 ml-auto w-[70%] text-gray-600 shadow-none">
-          <CardContent className="px-5 py-3">
-            Aさんとパフェを食べに行った。 先週私が誘ったやつ。美味しかった
-          </CardContent>
-        </Card>
-        <div className="mr-auto flex">
-          <IoPersonCircleSharp
-            size={"35px"}
-            color="gray"
-            className="mr-2 mt-2"
-          />
           {/* カード */}
-          <Card className="mb-5 w-[70%] text-gray-600 shadow-none">
+          <Card className="mb-5 ml-auto w-[70%] text-gray-600 shadow-none">
             <CardContent className="px-5 py-3">
-              なぜAさんを誘ったのですか？
+              Aさんとパフェを食べに行った。 先週私が誘ったやつ。美味しかった
             </CardContent>
           </Card>
+          <div className="mr-auto flex">
+            <IoPersonCircleSharp
+              size={"35px"}
+              color="gray"
+              className="mr-2 mt-2"
+            />
+            {/* カード */}
+            <Card className="mb-5 w-[70%] text-gray-600 shadow-none">
+              <CardContent className="px-5 py-3">
+                なぜAさんを誘ったのですか？
+              </CardContent>
+            </Card>
+          </div>
         </div>
-      </div>
       </div>
       <div className="flex w-full justify-around bg-white py-5">
         <Link href={"/setting"}>
--- src/components/resizeTextarea.tsx
+++ src/components/resizeTextarea.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 // 可変テキストエリア
-export default function ResizeTextarea(props: { className: string, text: string, onChange?: (newValue: string) => void, onKeyDown?: (newValue: string) => void }) {
-    const { className, text, onChange = () => { }, onKeyDown = () => { } } = props
-    const [value, setValue] = useState("");
+export default function ResizeTextarea(props: { className: string, text: string, onChange?: (newValue: string) => void }) {
+    const { className, text, onChange = () => { } } = props
     const textareaRef = useRef<HTMLTextAreaElement>(null)
 
     const resize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setValue(e.target.value)
         onChange(e.target.value)
         if (textareaRef.current) {
             textareaRef.current.style.height = "auto"
             textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
-        }
-    }
-
-    const returnValue = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            onKeyDown(value)
         }
     }
 
@@ -31,6 +22,6 @@ export default function ResizeTextarea(props: { className: string, text: string,
     }, [])
 
     return (
-        <textarea ref={textareaRef} rows={1} onChange={resize} onKeyDown={returnValue} className={className} style={{ resize: 'none', bottom: 0, overflow: "hidden" }} value={text} />
+        <textarea ref={textareaRef} rows={1} onChange={resize} className={className} style={{ resize: 'none', bottom: 0, overflow: "hidden" }} value={text} />
     )
 }
