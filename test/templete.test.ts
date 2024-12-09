c222a073f4e222a76be253b96b1a6c374ad1430c
uyuuuuu <soufflesumire.03@gmail.com>
2024-12-09 17:25:26 +0900

    test: create usertestの下準備

--- test/templete.test.ts
+++ test/templete.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { add } from "~/lib/templete";
+import { createNewUser } from "~/server/service/create";
 
 describe("add", () => {
   test("addが正常に動作すること", async () => {
@@ -10,4 +11,10 @@ describe("add", () => {
   //   const newTodo = { title: "" };
   //   await expect(add(1,-1)).rejects.toThrow();
   // });
+});
+
+describe("user test", () => {
+  test("create user", async () => {
+    await expect(createNewUser("s.oufflesumire03@gmail.com", "hashedPassword")).resolves.not.toBeNull();
+  });
 });
