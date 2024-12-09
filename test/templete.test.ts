efba0fb5111c432a18445466f94b2f9ddbe22501
uyuuuuu <soufflesumire.03@gmail.com>
2024-12-09 18:05:20 +0900

    test: createNewUser異常系

--- test/templete.test.ts
+++ test/templete.test.ts
@@ -13,8 +13,21 @@ describe("add", () => {
   // });
 });
 
-describe("user test", () => {
-  test("create user", async () => {
-    await expect(createNewUser("s.oufflesumire03@gmail.com", "hashedPassword")).resolves.not.toBeNull();
+describe("createNewUser", () => {
+  test("正常系", async () => {
+    const email = "xxx@gmail.com";
+    const hashedPassword = "pass";
+    const user = await createNewUser(email, hashedPassword);
+    expect(user).not.toBeNull();
+    expect(user?.id).toMatch(/^c[a-z0-9]{24}$/); //cuidの形式
+    expect(user?.email).toBe(email); // Emailが一致
+    expect(user?.password).toBe(hashedPassword); // パスワードが一致
+    expect(user?.emailVerified).toBeNull(); // 初期値がnull
+  });
+  test("異常系", async () => {
+    const email = "yyy@gmail.com";
+    const hashedPassword = "pass";
+    const sameuser = await createNewUser(email, hashedPassword);
+    await expect(createNewUser(email, hashedPassword)).resolves.toBeNull();
   });
 });
