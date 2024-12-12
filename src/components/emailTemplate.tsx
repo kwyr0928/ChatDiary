491fbe4f5dab20242d83765b2e092b0a93020037
uyuuuuu <soufflesumire.03@gmail.com>
2024-12-13 01:05:09 +0900

    update: メール本文修正

--- src/components/emailTemplate.tsx
+++ src/components/emailTemplate.tsx
@@ -5,7 +5,12 @@ interface EmailTemplateProps {
 
 export const EmailTemplate = ({ mail, token }: EmailTemplateProps) => (
   <div>
-    <h1>Welcome, {mail}!</h1>
+    <h1>ようこそ, {mail}さん!</h1>
+    <p>ChatDiaryに登録いただきありがとうございます！以下のリンクより、メールアドレス登録をお願いします。</p>
+    <p>リンクの有効期限は5分間です。リンクが切れていた場合、登録後の画面より「メールを再送」ボタンを押してください。</p>
     <a href={`http://localhost:3000/signup/complete?token=${token}`}>こちらをクリックで認証</a>
+    <br></br>
+    <p>このメールアドレスは送信専用のため、返信は受け付けておりません。</p>
+    <p>本メールに身に覚えの無い場合は、本メールを破棄していただきますようお願いいたします。</p>
   </div>
 );
