97b0c7985a943e7d9c1a8ccfa0f7d4b4cd82d422
niiroo <114127299+niiroo@users.noreply.github.com>
2024-12-13 02:31:38 +0900

    feat：FB機能実装（うまく動いてないかも）

--- src/server/service/create.ts
+++ src/server/service/create.ts
@@ -1,7 +1,9 @@
 import type { z } from "zod";
 import { type analysesSchema, type chatsSchema, type continuationSchema, type diariesSchema, type diaryTagsSchema, type monthlySummariesSchema, type newTag, userSchema } from "~/lib/schemas";
-import { getTodayContinuation } from "../repository/getdata";
+import { getDiariesByUserId, getDiaryData, getTodayContinuation } from "../repository/getdata";
 import { insertAnalyses, insertChat, insertContinuation, insertDiary, insertDiaryTag, insertMonthlySummaries, insertNewUser, insertTag } from "../repository/insertdata";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
 
 export async function createNewUser(email: string, hashedPassword: string) {
   try {
@@ -105,13 +107,48 @@ export async function connectDiaryTag(diaryId: string, tagId: string) {
 export async function createMonthlyFB(userId: string, target: number) {
   try {
     if (userId == null || target ==null) throw new Error("Invalid option data");
-    // text生成    
+    // 先月のまとめ生成
+    // @TODO: にいろ
 
+    // Gemini APIキーを設定
+    const apiKey = process.env.GEMINI_API_KEY;
 
+    if (!apiKey) throw new Error("err in getDiariesByUserId");
+    const genAI = new GoogleGenerativeAI(apiKey);
+    // モデルの取得
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { temperature: 1, }, });  // 使用モデル指定
 
+    // 全部の日記の本文を取得 
+    const diaries = await getDiariesByUserId(userId);
+    if (diaries == null) throw new Error("err in getDiariesByUserId");
 
-    
-    const text = "monthly feedback";
+    let diarySummaries = ""
+    if (diaries.length != 0) {
+      for (const diary of diaries) {
+        const diaryId: string = diary.id!;
+        const diaryData = await getDiaryData(diaryId);
+        if (diaryData == null) throw new Error("err in getDiaryData");
+
+        diarySummaries += "[" + diaryData.summary + "]";
+      }
+    }
+
+    // 日記全文＋要約の文章をGeminiに送る（チャットじゃなくてgenerateContents）
+    const prompt_post = diarySummaries + "あなたは要約と分析を得意とするアシスタントです。上記の[]で囲まれた先月の日記を基に、以下の点を要約して文章にしてください。 主な出来事や体験、感情についてどんな月だったかが把握できるように簡潔に200字程度でまとめてください。"
+
+    // テキスト生成
+    const result = await model.generateContent({
+      contents: [{ role: 'USER', parts: [{ text: prompt_post }] }],
+      generationConfig: { temperature: 2 },
+    });
+    // レスポンスの取得
+    const response = await result.response;
+    const generatedText = await response.text();
+
+    console.log(generatedText)
+    // FBの取得
+    const text = generatedText;    
+    // const text = "monthly feedback";
     const monthlySummariesData: z.infer<typeof monthlySummariesSchema> = {
       userId: userId,
       month: target,
@@ -130,13 +167,48 @@ export async function createMonthlyFB(userId: string, target: number) {
 export async function createAnalysesFB(userId: string) {
   try {
     if (userId == null) throw new Error("Invalid option data");
-    // text生成
+    // 全体FB生成
     // @TODO: にいろ
 
-    // 一か月の日記の本文を取得 
-    // 全文＋要約の文章をGeminiに送る（チャットじゃなくてgenerateContents？）
+    // Gemini APIキーを設定
+    const apiKey = process.env.GEMINI_API_KEY;
 
-    const text = "analyses feedback";
+    if (!apiKey) throw new Error("err in getDiariesByUserId");
+    
+    const genAI = new GoogleGenerativeAI(apiKey);
+    // モデルの取得
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { temperature: 1, }, });  // 使用モデル指定
+
+    // 全部の日記の本文を取得 
+    const diaries = await getDiariesByUserId(userId);
+    if (diaries == null) throw new Error("err in getDiariesByUserId");
+
+    let diarySummaries = ""
+    if (diaries.length != 0) {
+      for (const diary of diaries) {
+        const diaryId: string = diary.id!;
+        const diaryData = await getDiaryData(diaryId);
+        if (diaryData == null) throw new Error("err in getDiaryData");
+
+        diarySummaries += "[" + diaryData.summary + "]";
+      }
+    }
+
+    // 日記全文＋要約の文章をGeminiに送る（チャットじゃなくてgenerateContents）
+    const prompt_post = diarySummaries + "あなたは人物の分析を得意とするアシスタントです。上記の[]で囲まれた日記を基に、以下の点を要約して文章にしてください。 主な出来事や体験、感情の変化や価値観、ユーザーの強みや特徴を簡潔かつ自己分析に役立つ形で200字程度でまとめてください。"
+
+    // テキスト生成
+    const result = await model.generateContent({
+      contents: [{ role: 'USER', parts: [{ text: prompt_post }] }],
+      generationConfig: { temperature: 2 },
+    });
+    // レスポンスの取得
+    const response = await result.response;
+    const generatedText = await response.text();
+
+    console.log(generatedText)
+    // FBの取得
+    const text = generatedText;
 
     const analysesData: z.infer<typeof analysesSchema> = {
       userId: userId,
--- src/server/service/update.ts
+++ src/server/service/update.ts
@@ -8,15 +8,14 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function updateAnalysesFB(userId: string) {
   try {
     if (userId == null) throw new Error("Invalid option data");
-    // text生成
+    // 全体FB更新
     // @TODO: にいろ
 
     // Gemini APIキーを設定
     const apiKey = process.env.GEMINI_API_KEY;
 
-    if (!apiKey) {
-      return NextResponse.json({ error: 'API key not found' }, { status: 500 });
-    }
+    if (!apiKey) throw new Error("err in getDiariesByUserId");
+
     const genAI = new GoogleGenerativeAI(apiKey);
     // モデルの取得
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { temperature: 1, }, });  // 使用モデル指定
@@ -36,20 +35,22 @@ export async function updateAnalysesFB(userId: string) {
       }
     }
 
-    // 全文＋要約の文章をGeminiに送る（チャットじゃなくてgenerateContents？）
-    const prompt_post = diarySummaries + ""
+    // 日記全文＋要約の文章をGeminiに送る（チャットじゃなくてgenerateContents？）
+    const prompt_post = diarySummaries + "あなたは人物の分析を得意とするアシスタントです。上記の[]で囲まれた日記を基に、以下の点を要約して文章にしてください。 主な出来事や体験、感情の変化や価値観、ユーザーの強みや特徴を簡潔かつ自己分析に役立つ形で200字程度でまとめてください。"
+
     // テキスト生成
     const result = await model.generateContent({
       contents: [{ role: 'USER', parts: [{ text: prompt_post }] }],
-      generationConfig: { maxOutputTokens: 100 },
+      generationConfig: { temperature: 2 },
     });
     // レスポンスの取得
     const response = await result.response;
     const generatedText = await response.text();
 
     console.log(generatedText)
-
-    const text = "analyses feedback";
+    // FBの取得
+    const text = generatedText;
+    // const text = "analyses feedback";
     const analysesData: z.infer<typeof analysesSchema> = {
       userId: userId,
       text: text,
