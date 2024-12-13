d34df22fe5126c22966d6aa66f4046bb822513f2
うゆう <110881705+uyuuuuu@users.noreply.github.com>
2024-12-13 20:05:06 +0900

    Revert "update: メールのurlをローカルリモートに両方対応"

--- src/components/emailTemplate.tsx
+++ src/components/emailTemplate.tsx
@@ -8,7 +8,7 @@ export const EmailTemplate = ({ mail, token }: EmailTemplateProps) => (
     <h1>ようこそ, {mail}さん!</h1>
     <p>ChatDiaryに登録いただきありがとうございます！以下のリンクより、メールアドレス登録をお願いします。</p>
     <p>リンクの有効期限は5分間です。リンクが切れていた場合、登録後の画面より「メールを再送」ボタンを押してください。</p>
-    <a href={`${process.env.AUTH_URL}/signup/complete?token=${token}`}>こちらをクリックで認証</a>
+    <a href={`http://localhost:3000/signup/complete?token=${token}`}>こちらをクリックで認証</a>
     <br></br>
     <p>このメールアドレスは送信専用のため、返信は受け付けておりません。</p>
     <p>本メールに身に覚えの無い場合は、本メールを破棄していただきますようお願いいたします。</p>
